151517bbb8dabe86349186e3abc2ea2b49315cc9
davidstellini <davste93@gmail.com>
2022-08-16 21:18:54 +0300

    Deploy test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { SecondPageComponent } from './second-page/second-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, SecondPageComponent, HeaderComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/angular-partytown-yandex-repro/'}]
 })
 export class AppModule { }
